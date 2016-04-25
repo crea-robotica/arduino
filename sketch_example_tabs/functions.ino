bb261774dfb30c7b75b8639e685e31800c9463b8
Raúl Pérula-Martínez <raules@gmail.com>
2016-04-25 18:23:59 +0200

    updating examples

---
+++ sketch_example_tabs/functions.ino
@@ -0,0 +1,10 @@
+/**
+ * @brief Auxiliar function to print a string.
+ * @para s: The input string
+ * @return: Nothing
+ */
+void print_str(String s)
+{
+    // show the message in the serial monitor
+    Serial.println(s);
+}
