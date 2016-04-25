e5b90ae9d6a9f409cf5db72725e92b95c42d9178
Raúl Pérula-Martínez <raules@gmail.com>
2016-04-25 18:14:56 +0200

    updating examples

---
+++ sketch_example_tabs/sketch_example_tabs.ino
@@ -0,0 +1,52 @@
+/**
+ * @brief Complete sketch example
+ * @author CREA Robotica Educativa
+ * @license GPLv3
+ *
+ * https://crea-robotica.com/
+ */
+
+// includes
+// example: #include <Servo.h>
+
+// global variables 
+int time_waiting = 500;
+String msg = "Hello world.";
+
+// functions
+
+/**
+ * @brief Auxiliar function to print a string.
+ * @para s: The input string
+ * @return: Nothing
+ */
+void print_str(String s)
+{
+    // show the message in the serial monitor
+    Serial.println(s);
+}
+
+/**
+ * @brief setup routine
+ */
+void setup()
+{
+    // open serial port to 9600 bps
+    Serial.begin(9600);
+    
+    // empty serial port
+    Serial.flush();
+}
+
+/**
+ * @brief loop routine
+ */
+void loop()
+{
+    // using the function
+    print_str(msg);
+    
+    // wait 500 ms
+    delay(time_waiting);
+}
+
