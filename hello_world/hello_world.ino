4557be66fde600681d97ad05f41fc63343b036e0
Raúl Pérula-Martínez <raules@gmail.com>
2016-04-25 17:46:24 +0200

    first commit

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,26 @@
+/**
+ * @brief Hello World
+ * @author CREA Robotica Educativa
+ * @license GPLv3
+ */
+
+// the setup routine runs once when you press reset:
+void setup()
+{                
+    // open serial port to 9600 bps
+    Serial.begin(9600);
+    
+    // empty serial port
+    Serial.flush();
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+    // show the message in the serial monitor
+    Serial.println("Hello world.");
+    
+    // wait 500 ms
+    delay(500);
+}
+
